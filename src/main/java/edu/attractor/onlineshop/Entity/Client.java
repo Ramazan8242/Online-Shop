0e342f141f199acaa3abc5925a57f5a3e1bc466b
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-06-14 15:39:05 +0600

    Added validations to the Client class

--- src/main/java/edu/attractor/onlineshop/Entity/Client.java
+++ src/main/java/edu/attractor/onlineshop/Entity/Client.java
@@ -6,6 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -17,13 +21,15 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    @NotNull
     @Column(length = 128)
+    @Size(min = 3,max = 90)
     private String name;
-
+    @Min(13)
+    private Integer age;
     @Column(length = 224)
     private String address;
-
+    @Email
     @Column(unique = true)
     private String email;
 }
--- src/main/resources/db/migration/V1000__onlineshop_tables.sql
+++ src/main/resources/db/migration/V1000__onlineshop_tables.sql
@@ -1,8 +1,9 @@
 use `onlineshop`;
 
 create table clients(
-id integer(11) not null AUTO_INCREMENT,
+id integer(90) not null AUTO_INCREMENT,
 name varchar (128) not null,
+age integer not null,
 address varchar (128) not null,
 email varchar (128) not null,
 primary key (id)
@@ -14,7 +15,7 @@ primary key (id)
 );
 
 create table products(
-id integer(11) not null AUTO_INCREMENT,
+id integer(90) not null AUTO_INCREMENT,
 name varchar (128) not null,
 primary key (id)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
