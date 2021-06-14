d0b40a42cc3d748bcf9e236b5d2cbe527e1e9c14
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-06-14 15:42:34 +0600

    Added validations to the Product class

--- src/main/java/edu/attractor/onlineshop/Entity/Product.java
+++ src/main/java/edu/attractor/onlineshop/Entity/Product.java
@@ -3,6 +3,8 @@ package edu.attractor.onlineshop.Entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -14,7 +16,8 @@ public class Product{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    @NotNull
+    @Size(min = 3,max = 90)
     @Column(name ="name",length = 128)
     private String name;
 
