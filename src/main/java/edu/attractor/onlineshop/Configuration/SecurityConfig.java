f4b73afbeda7754696d84f796d284b9d66988965
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-06-21 15:32:03 +0600

    Connected Spring Security

--- src/main/java/edu/attractor/onlineshop/Configuration/SecurityConfig.java
+++ src/main/java/edu/attractor/onlineshop/Configuration/SecurityConfig.java
@@ -1,4 +1,12 @@
 package edu.attractor.onlineshop.Configuration;
 
-public class SecurityConfig {
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
 }
