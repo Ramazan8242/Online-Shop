996bdbc7b3e72209d4dd6fe3e01094d8de50cf5f
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-06-21 15:34:26 +0600

    Added the necessary methods to the class SecurityConfig

--- src/main/java/edu/attractor/onlineshop/Configuration/SecurityConfig.java
+++ src/main/java/edu/attractor/onlineshop/Configuration/SecurityConfig.java
@@ -2,11 +2,37 @@ package edu.attractor.onlineshop.Configuration;
 
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final DataSource dataSource;
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String fetchUsersQuery = "select email, password, enabled"
+                + " from customers"
+                + " where email = ?";
+
+        String fetchRolesQuery = "select email, role"
+                + " from customers"
+                + " where email = ?";
+
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery(fetchUsersQuery)
+                .authoritiesByUsernameQuery(fetchRolesQuery)
+                .dataSource(dataSource);
+    }
 }
