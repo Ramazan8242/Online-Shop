c7bb920d8119e2c2d9996324289a6746e5cd1f8b
Ramazan Tulanbaev <ramazan.tulanbaev@bk.ru>
2021-06-24 14:11:11 +0600

    Added methods from the previous project

--- src/main/java/edu/attractor/onlineshop/Controller/ClientController.java
+++ src/main/java/edu/attractor/onlineshop/Controller/ClientController.java
@@ -1,5 +1,6 @@
 package edu.attractor.onlineshop.Controller;
 
+import edu.attractor.onlineshop.Dto.KeyValueRequestDto;
 import edu.attractor.onlineshop.Exeption.ClientRegistrationForm;
 import edu.attractor.onlineshop.Entity.Client;
 import edu.attractor.onlineshop.Service.ClientService;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Data
@@ -107,5 +109,38 @@ public class ClientController {
     public String emptyCart(HttpSession session) {
         session.removeAttribute(Constants.CART_ID);
         return "redirect:/cart";
+    }
+    @GetMapping("/")
+    public String index(Model model, HttpSession session) {
+        var map = new HashMap<String, Object>();
+        map.put("Идентификатор сессии", session.getId());
+
+        session.getAttributeNames()
+                .asIterator()
+                .forEachRemaining(key -> map.put(key, session.getAttribute(key).toString()));
+
+        model.addAttribute("sessionAttributes", map);
+        return "index";
+    }
+
+    @GetMapping("/invalidate")
+    public String invalidate(HttpSession session) {
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/store")
+    public String store(KeyValueRequestDto data, HttpSession session) {
+        if (session != null) {
+            var attr = session.getAttribute(data.getKey());
+            if (attr == null) {
+                session.setAttribute(data.getKey(), data.getValue());
+            }
+        }
+
+        return "redirect:/";
     }
 }
